705f3bc3c69ad2de7e49f8786c3240f23d0fd426
Ken Le <ken@ryukyu-i.co.jp>
2017-07-14 18:31:37 +0700

    Added pagination.

--- javascripts/controllers/home_panel_controller.js
+++ javascripts/controllers/home_panel_controller.js
@@ -37,20 +37,47 @@ app.controller('homePanelController', function($scope, $http, $localStorage, Com
         }
     }
 
-    $scope.statuses = [
+    $scope.filter_statuses = [
         "all",
         "new",
         "doing",
         "finished"
     ]
 
-    $scope.task_status = $scope.statuses[0];
-    $scope.project_status = $scope.statuses[0];
+    $scope.limits = ["5","10","15","20","all"];
+    $scope.tasks_limit = $scope.limits[1];
+    $scope.projects_limit = $scope.limits[1];
+
+    $scope.tasks_current_page = 1;
+    $scope.projects_current_page = 1;
+
+    $scope.selected_tasks_status_filter = $scope.filter_statuses[0];
+    $scope.selected_projects_status_filter = $scope.filter_statuses[0];
+
+    $scope.currentTasksPageClass = function(page) {
+        if(page == $scope.tasks_current_page) {
+            return "selected";
+        } else {
+            return "";
+        }
+    }
+
+    $scope.currentProjectsPageClass = function(page) {
+        if(page == $scope.projects_current_page) {
+            return "selected";
+        } else {
+            return "";
+        }
+    }
 
     $scope.cssLast = function(isLast) {
         if(isLast) {
             return "last";
         }
+    }
+
+    var getNumber = function(num) {
+        return new Array(num);   
     }
 
     $scope.checkPriorityImportance = function(priority) {
@@ -73,32 +100,243 @@ app.controller('homePanelController', function($scope, $http, $localStorage, Com
         }
     }
 
-    $scope.getTasks = function(status) {
+    $scope.getTasks = function(status,limit,page) {
+        $scope.tasks_current_page = page;
+
+        var queryStr = "";
+
+        if(status && status != "all") {
+            queryStr = "?status="+$status;
+        }
+
+        //Get total tasks count first in order to calculate pagination parameters
+        $http({
+            method: 'GET',
+            url: tasksApiBaseURL + '/tasks-count' + queryStr, //'/ranked-tasks',
+            headers: {
+                'x-access-token': CommonFunctions.getToken()
+            }
+        }).then(function (response) {
+            $scope.tasks_count = parseInt(response.data);
+
+            //For pagination
+            if(limit != "all" && $scope.tasks_count && $scope.tasks_count > 0) {
+                var tasks_page_count = Math.ceil($scope.tasks_count / parseInt(limit));
+                $scope.tasks_page_count_arr = getNumber(tasks_page_count);
+            } else {
+                $scope.tasks_page_count_arr = getNumber(1);
+            }
+
+            //Set the current page
+            var pageInt = parseInt(page,10);
+            if(pageInt) {
+                if(pageInt <= 0) { //don't let pages go below zero
+                    $scope.tasks_current_page = 1;
+                } else if (pageInt > $scope.tasks_page_count_arr.length) { //don't let pages go past max pages
+                    $scope.tasks_current_page = $scope.tasks_page_count_arr.length;
+                } else {
+                    $scope.tasks_current_page = pageInt;
+                }
+            } else {
+                $scope.tasks_current_page = 1;
+            }
+
+            //Build the query string to get the tasks for the current page
+            var queryArr = [];
+            var queryStr = "";
+            var queryStatus = null;
+            var queryLimit = null;
+            var queryPage = null;
+
+            //Query to filter by status
+            if(status && status != "all") {
+                queryStatus = "status="+status;
+                queryArr.push(queryStatus);
+            }
+
+            //Query for limiting results and pagination
+            if(limit && limit != "all") {
+                queryLimit = "limit="+limit;
+                queryArr.push(queryLimit);
+                queryPage = "page="+$scope.tasks_current_page;
+                queryArr.push(queryPage);
+            }
+
+            if(queryArr.length > 0) {
+                queryStr = "?" + queryArr.join("&");
+            }
+
+            //Get tasks list        
+            $http({
+                method: 'GET',
+                url: tasksApiBaseURL + '/tasks' + queryStr, //'/ranked-tasks',
+                headers: {
+                    'x-access-token': CommonFunctions.getToken()
+                }
+            }).then(function (response) {
+                $scope.tasks = response.data;
+
+                for(var i=0;i<$scope.tasks.length;i++) {
+                    var current_task = $scope.tasks[i];
+                    var current_project_id = current_task.project_id;
+
+                    //Get the project name
+                    if(current_project_id) {
+                        $http({
+                            method: 'GET',
+                            url: projectsApiBaseURL + '/projects/'+current_project_id,
+                            headers: {
+                                'x-access-token': CommonFunctions.getToken()
+                            },
+                            params: {
+                                'i': i
+                            }
+                        }).then(function (response) {
+                            var current_project = response.data;
+                            $scope.tasks[parseInt(response.config["params"]["i"])]["project_name"] = current_project.name;
+                        });                    
+                    }
+
+                    //Get all users on the current task
+                    $http({
+                        method: 'GET',
+                        url: tasksApiBaseURL + '/tasks/'+current_task.id+'/users',
+                        headers: {
+                           'x-access-token': CommonFunctions.getToken()
+                        },
+                        params: {
+                           'i': i
+                        }
+                    }).then(function (response) {
+                        $scope.tasks[parseInt(response.config["params"]["i"])]["users"] = response.data
+
+                        for(var j=0;j<response.data.length;j++) {
+                            $http({
+                                method: 'GET',
+                                url: usersApiBaseURL + '/users/'+response.data[j]["user_id"],
+                                headers: {
+                                    'x-access-token': CommonFunctions.getToken()
+                                },
+                                params: {
+                                    'i': response.config["params"]["i"],
+                                    'j': j
+                                }
+                            }).then(function (response) {
+                                var i = parseInt(response.config["params"]["i"]);
+                                var j = parseInt(response.config["params"]["j"]);
+                                $scope.tasks[i]["users"][j].firstname = response.data.firstname; 
+                                $scope.tasks[i]["users"][j].lastname = response.data.lastname; 
+                            });                          
+                        }
+
+                    });                   
+                }
+            }); 
+        });
+    }//End getTasks()
+
+    $scope.getProjectsCount = function(status) {
         var queryStr = "";
 
         if(status && status != "all") {
             queryStr = "?status="+status;
         }
 
-        //Get tasks list        
+        //Get projects count
         $http({
             method: 'GET',
-            url: tasksApiBaseURL + '/tasks' + queryStr, //'/ranked-tasks',
+            url: projectsApiBaseURL + '/projects-count' + queryStr, //'/ranked-projects',
             headers: {
                 'x-access-token': CommonFunctions.getToken()
             }
         }).then(function (response) {
-            $scope.tasks = response.data;
+            $scope.projects_count = parseInt(response.data);
+        });
+    }
 
-            for(var i=0;i<$scope.tasks.length;i++) {
-                var current_task = $scope.tasks[i];
-                var current_project_id = current_task.project_id;
+    $scope.getProjects = function(status,limit,page) {
+        $scope.projects_current_page = page;
 
-                //Get the project name
-                if(current_project_id) {
+        var queryStr = "";
+
+        if(status && status != "all") {
+            queryStr = "?status="+$status;
+        }
+
+        //Get total projects count first in order to calculate pagination parameters
+        $http({
+            method: 'GET',
+            url: projectsApiBaseURL + '/projects-count' + queryStr, //'/ranked-projects',
+            headers: {
+                'x-access-token': CommonFunctions.getToken()
+            }
+        }).then(function (response) {
+            $scope.projects_count = parseInt(response.data);
+
+            //For pagination
+            if(limit != "all" && $scope.projects_count && $scope.projects_count > 0) {
+                var projects_page_count = Math.ceil($scope.projects_count / parseInt(limit));
+                $scope.projects_page_count_arr = getNumber(projects_page_count);
+            } else {
+                $scope.projects_page_count_arr = getNumber(1);
+            }
+
+            //Set the current page
+            var pageInt = parseInt(page,10);
+            if(pageInt) {
+                if(pageInt <= 0) { //don't let pages go below zero
+                    $scope.projects_current_page = 1;
+                } else if (pageInt > $scope.projects_page_count_arr.length) { //don't let pages go past max pages
+                    $scope.projects_current_page = $scope.projects_page_count_arr.length;
+                } else {
+                    $scope.projects_current_page = pageInt;
+                }
+            } else {
+                $scope.projects_current_page = 1;
+            }
+
+            //Build the query string to get the projects for the current page
+            var queryArr = [];
+            var queryStr = "";
+            var queryStatus = null;
+            var queryLimit = null;
+            var queryPage = null;
+
+            //Query to filter by status
+            if(status && status != "all") {
+                queryStatus = "status="+status;
+                queryArr.push(queryStatus);
+            }
+
+            //Query for limiting results and pagination
+            if(limit && limit != "all") {
+                queryLimit = "limit="+limit;
+                queryArr.push(queryLimit);
+                queryPage = "page="+$scope.projects_current_page;
+                queryArr.push(queryPage);
+            }
+
+            if(queryArr.length > 0) {
+                queryStr = "?" + queryArr.join("&");
+            }
+
+            //Get projects list
+            $http({
+                method: 'GET',
+                url: projectsApiBaseURL + '/projects' + queryStr,
+                headers: {
+                    'x-access-token': CommonFunctions.getToken()
+                }
+            }).then(function (response) {
+                $scope.projects = response.data;
+
+                for(var i=0;i<response.data.length;i++) {
+                    var current_project = $scope.projects[i];
+                    var current_project_id = current_project.id;
+
                     $http({
                         method: 'GET',
-                        url: projectsApiBaseURL + '/projects/'+current_project_id,
+                        url: projectsApiBaseURL + '/projects/'+current_project_id+'/users',
                         headers: {
                             'x-access-token': CommonFunctions.getToken()
                         },
@@ -106,117 +344,45 @@ app.controller('homePanelController', function($scope, $http, $localStorage, Com
                             'i': i
                         }
                     }).then(function (response) {
-                        var current_project = response.data;
-                        $scope.tasks[parseInt(response.config["params"]["i"])]["project_name"] = current_project.name;
+                        $scope.projects[parseInt(response.config["params"]["i"])]["users"] = response.data;
+
+                        for(var j=0;j<response.data.length;j++) {
+                            $http({
+                                method: 'GET',
+                                url: usersApiBaseURL + '/users/'+response.data[j]["user_id"],
+                                headers: {
+                                    'x-access-token': CommonFunctions.getToken()
+                                },
+                                params: {
+                                    'i': response.config["params"]["i"],
+                                    'j': j
+                                }
+                            }).then(function (response) {
+                                var i = parseInt(response.config["params"]["i"]);
+                                var j = parseInt(response.config["params"]["j"]);
+                                $scope.projects[i]["users"][j].firstname = response.data.firstname; 
+                                $scope.projects[i]["users"][j].lastname = response.data.lastname; 
+                            });                          
+                        }
+                    });                    
+
+                    //Get the project's tasks, limit top 3
+                    $http({
+                        method: 'GET',
+                        url: tasksApiBaseURL + '/projects/'+current_project_id+'/tasks?limit=3',
+                        headers: {
+                            'x-access-token': CommonFunctions.getToken()
+                        },
+                        params: {
+                            'i': i
+                        }
+                    }).then(function (response) {
+                        $scope.projects[parseInt(response.config["params"]["i"])]["tasks"] = response.data;
                     });                    
                 }
-
-                //Get all users on the current task
-                $http({
-                    method: 'GET',
-                    url: tasksApiBaseURL + '/tasks/'+current_task.id+'/users',
-                    headers: {
-                       'x-access-token': CommonFunctions.getToken()
-                    },
-                    params: {
-                       'i': i
-                    }
-                }).then(function (response) {
-                    $scope.tasks[parseInt(response.config["params"]["i"])]["users"] = response.data
-
-                    for(var j=0;j<response.data.length;j++) {
-                        $http({
-                            method: 'GET',
-                            url: usersApiBaseURL + '/users/'+response.data[j]["user_id"],
-                            headers: {
-                                'x-access-token': CommonFunctions.getToken()
-                            },
-                            params: {
-                                'i': response.config["params"]["i"],
-                                'j': j
-                            }
-                        }).then(function (response) {
-                            var i = parseInt(response.config["params"]["i"]);
-                            var j = parseInt(response.config["params"]["j"]);
-                            $scope.tasks[i]["users"][j].firstname = response.data.firstname; 
-                            $scope.tasks[i]["users"][j].lastname = response.data.lastname; 
-                        });                          
-                    }
-
-                });                   
-            }
-        }); 
-    }
-
-    $scope.getProjects = function(status) {
-        var queryStr = "";
-
-        if(status && status != "all") {
-            queryStr = "?status="+status;
-        }
-
-        //Get projects list
-        $http({
-            method: 'GET',
-            url: projectsApiBaseURL + '/projects' + queryStr,
-            headers: {
-                'x-access-token': CommonFunctions.getToken()
-            }
-        }).then(function (response) {
-            $scope.projects = response.data;
-
-            for(var i=0;i<response.data.length;i++) {
-                var current_project = $scope.projects[i];
-                var current_project_id = current_project.id;
-
-                $http({
-                    method: 'GET',
-                    url: projectsApiBaseURL + '/projects/'+current_project_id+'/users',
-                    headers: {
-                        'x-access-token': CommonFunctions.getToken()
-                    },
-                    params: {
-                        'i': i
-                    }
-                }).then(function (response) {
-                    $scope.projects[parseInt(response.config["params"]["i"])]["users"] = response.data;
-
-                    for(var j=0;j<response.data.length;j++) {
-                        $http({
-                            method: 'GET',
-                            url: usersApiBaseURL + '/users/'+response.data[j]["user_id"],
-                            headers: {
-                                'x-access-token': CommonFunctions.getToken()
-                            },
-                            params: {
-                                'i': response.config["params"]["i"],
-                                'j': j
-                            }
-                        }).then(function (response) {
-                            var i = parseInt(response.config["params"]["i"]);
-                            var j = parseInt(response.config["params"]["j"]);
-                            $scope.projects[i]["users"][j].firstname = response.data.firstname; 
-                            $scope.projects[i]["users"][j].lastname = response.data.lastname; 
-                        });                          
-                    }
-                });                    
-
-                //Get the project's tasks, limit top 3
-                $http({
-                    method: 'GET',
-                    url: tasksApiBaseURL + '/projects/'+current_project_id+'/tasks?limit=3',
-                    headers: {
-                        'x-access-token': CommonFunctions.getToken()
-                    },
-                    params: {
-                        'i': i
-                    }
-                }).then(function (response) {
-                    $scope.projects[parseInt(response.config["params"]["i"])]["tasks"] = response.data;
-                });                    
-            }
+            });
         });
-    }
+    } //End getProjects()
 
     if($localStorage.loggedin_user) {
         //Get users list
@@ -230,9 +396,9 @@ app.controller('homePanelController', function($scope, $http, $localStorage, Com
             $scope.users = response.data;
         });
 
-        $scope.getProjects($scope.project_status);
+        $scope.getProjects($scope.selected_projects_status_filter, $scope.projects_limit, $scope.projects_current_page);
 
-        $scope.getTasks($scope.task_status);
+        $scope.getTasks($scope.selected_tasks_status_filter, $scope.tasks_limit, $scope.tasks_current_page);
     } 
 
 });
