19238cd5fb0bd291dfe0050c36b0af514d7f7df3
Ken Le <ken@ryukyu-i.co.jp>
2017-07-28 14:20:00 +0700

    Added routes for different modes

--- javascripts/config/routes.js
+++ javascripts/config/routes.js
@@ -1,17 +1,36 @@
 var app = angular.module('myApp', ['ngRoute','ngStorage','ngSanitize','btford.markdown']);
 
-var local_development_mode = false;
+var development_mode = 'production';
 
-var usersApiBaseURL = "https://ryukyu-social.cleverword.com/users_service/api";
-var userPhotosApiBaseURL = "https://ryukyu-social.cleverword.com/user_photos_service/api";
-var projectsApiBaseURL = "https://ryukyu-social.cleverword.com/projects_service/api";
-var tasksApiBaseURL = "https://ryukyu-social.cleverword.com/tasks_service/api";
+var usersApiBaseURL = null;
+var userPhotosApiBaseURL = null;
+var projectsApiBaseURL = null;
+var tasksApiBaseURL = null;
 
-if(local_development_mode) {
-  usersApiBaseURL = "http://localhost:5000";
-  userPhotosApiBaseURL = "http://localhost:5001";
-  projectsApiBaseURL = "http://localhost:5002";
-  tasksApiBaseURL = "http://localhost:5003";
+switch (development_mode) {
+  case 'local_development':
+    usersApiBaseURL = "http://localhost:7000";
+    userPhotosApiBaseURL = "http://localhost:7001";
+    projectsApiBaseURL = "http://localhost:7002";
+    tasksApiBaseURL = "http://localhost:7003";
+    break;
+  case 'remote_development':
+    usersApiBaseURL = "https://ryukyu-social-dev.cleverword.com/users_service/api";
+    userPhotosApiBaseURL = "https://ryukyu-social-dev.cleverword.com/user_photos_service/api";
+    projectsApiBaseURL = "https://ryukyu-social-dev.cleverword.com/projects_service/api";
+    tasksApiBaseURL = "https://ryukyu-social-dev.cleverword.com/tasks_service/api";  
+    break;
+  case 'production':
+    usersApiBaseURL = "https://ryukyu-social.cleverword.com/users_service/api";
+    userPhotosApiBaseURL = "https://ryukyu-social.cleverword.com/user_photos_service/api";
+    projectsApiBaseURL = "https://ryukyu-social.cleverword.com/projects_service/api";
+    tasksApiBaseURL = "https://ryukyu-social.cleverword.com/tasks_service/api";
+    break;
+  default:
+    usersApiBaseURL = "http://localhost:7000";
+    userPhotosApiBaseURL = "http://localhost:7001";
+    projectsApiBaseURL = "http://localhost:7002";
+    tasksApiBaseURL = "http://localhost:7003";  
 }
 
 app.service('CommonFunctions', function() {
