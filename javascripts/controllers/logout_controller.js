f7cf36e2e491309bb56cf5ee3227d76cfdad57e2
Ken Le <ken@ryukyu-i.co.jp>
2017-08-10 17:32:52 +0700

    Added notifications and some refactoring

--- javascripts/controllers/logout_controller.js
+++ javascripts/controllers/logout_controller.js
@@ -1,10 +1,13 @@
-app.controller('logoutController', function($scope, $http, $route, $location, $localStorage, CommonFunctions) {
+app.controller('logoutController', function($scope, $http, $route, $location, $localStorage, CommonFunctions, SiteNotifications) {
     //delete $localStorage.token;
     CommonFunctions.deleteToken(); 
     delete $localStorage.loggedin_user; //decoded token
     delete $localStorage.last_visited_user_id;
     delete $localStorage.last_visited_project_id;
     delete $localStorage.url_attempted;
+
+    SiteNotifications.unSubscribeToNotifications();
+
     $localStorage.flash_message = "Logged out";
     $scope.$parent.flash_level = "alert";
     $location.path('/');
