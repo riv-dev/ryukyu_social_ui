2c6d3e6b5a679fae214b12ef860e2e919b33183b
huyenrk <phan@ryukyu-i.co.jp>
2017-08-14 17:11:03 +0700

    update

--- javascripts/controllers/edit_user_profile_controller.js
+++ javascripts/controllers/edit_user_profile_controller.js
@@ -2,13 +2,60 @@ app.controller('editUserProfileController', function($scope, $http, $location, $
     CommonFunctions.setFlashMessage($scope, $localStorage);
     CommonFunctions.checkLoggedInUser($scope, $localStorage, $location);
 
-    $scope.not_admin = function() {
-        if($localStorage.loggedin_user && $localStorage.loggedin_user.admin) {
-            return false;
-        } else {
-            return true;
+    $scope.back = function() {
+        window.history.back();
+    }
+    //Get current user_profile
+    $http({
+        method: 'GET',
+        url: userProfileApiBaseURL + '/users/' + $routeParams.user_id,
+        headers: {
+            'x-access-token': CommonFunctions.getToken()
+        },
+    }).then(function (response) {
+        $scope.nickname = response.data.nickname;
+        $scope.phone_number = response.data.phone_number;
+        $scope.birthday = response.data.birthday;
+        $scope.status = response.data.status;
+        $scope.bio = response.data.bio;
+
+        
+        if($routeParams.project_id) {
+            $scope.project_id = parseInt($routeParams.project_id);
+        }
+    });
+
+    $scope.put = function(){
+        if ($scope.not_exist()){
+            $http({
+                method: 'PUT',
+                url: usersApiBaseURL + '/users/' + $routeParams.user_id + '/password',
+                headers: {
+                    'x-access-token': CommonFunctions.getToken()
+                },
+                data: {password: $scope.password}
+            }).then(
+                function successCallback(response) {
+                    $localStorage.flash_message = "Successfully updated password!";
+                    $scope.$parent.flash_level = "success";
+                    window.history.back();
+                },
+                function errorCallback(response) {
+                    $scope.$parent.flash_message = "Error updating password.";
+                    $scope.$parent.flash_level = "fail";
+                    $scope.errors = {};
+                    var responseError;
+                    for (var i=0; i < response.data.errors.length; i++) {
+                        responseError = response.data.errors[i];
+                        if(responseError.hasOwnProperty('param')) {
+                            if(!$scope.errors[responseError['param']]) {
+                                $scope.errors[responseError['param']] = [];
+                            }
+                            $scope.errors[responseError['param']].push(responseError['msg']);
+                        }
+                    }
+                }
+            );
         }
     }
-    //Only admin can edit user information
-    //User's can only edit their user profile, which is in a different microservice
 });
