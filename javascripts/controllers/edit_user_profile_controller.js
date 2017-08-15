7b89c13cd38ef7214c0091227b89124db8e64a4e
huyenrk <phan@ryukyu-i.co.jp>
2017-08-15 10:10:41 +0700

    blah

--- javascripts/controllers/edit_user_profile_controller.js
+++ javascripts/controllers/edit_user_profile_controller.js
@@ -2,9 +2,16 @@ app.controller('editUserProfileController', function($scope, $http, $location, $
     CommonFunctions.setFlashMessage($scope, $localStorage);
     CommonFunctions.checkLoggedInUser($scope, $localStorage, $location);
 
-    $scope.back = function() {
-        window.history.back();
-    }
+
+    if($localStorage.loggedin_user){
+        $scope.$parent.hero = "Edit User Profile";
+        $scope.this_user_id = $routeParams.user_id;
+
+        $scope.back = function() {
+            window.history.back();
+        }
+    
+        
     //Get current user_profile
     $http({
         method: 'GET',
@@ -13,49 +20,102 @@ app.controller('editUserProfileController', function($scope, $http, $location, $
             'x-access-token': CommonFunctions.getToken()
         },
     }).then(function (response) {
-        $scope.nickname = response.data.nickname;
-        $scope.phone_number = response.data.phone_number;
-        $scope.birthday = response.data.birthday;
-        $scope.status = response.data.status;
-        $scope.bio = response.data.bio;
+        if(response.data.user_id) {
+            //Get the profile details to fill in form
+            $scope.nickname = response.data.nickname;
+            $scope.phone_number = response.data.phone_number;
+            $scope.birthday = response.data.birthday;
+            $scope.status = response.data.status;
+            $scope.bio = response.data.bio;
 
-        
-        if($routeParams.project_id) {
-            $scope.project_id = parseInt($routeParams.project_id);
+            $scope.submit = function() {
+                $http({
+                    method: 'PUT',
+                    url: userProfileApiBaseURL + '/users/' + $routeParams.user_id,
+                    headers: {
+                        'x-access-token': CommonFunctions.getToken()
+                    },
+                    data: {
+                        nickname: $scope.nickname, 
+                        phone_number: $scope.phone_number, 
+                        birthday: $scope.birthday, 
+                        status: $scope.status, 
+                        bio: $scope.bio
+                    } 
+                }).then(
+                    function successCallback(response) {
+                        $localStorage.flash_message = "User Profile Updated!";
+                        $scope.$parent.flash_level = "success";
+                        window.history.back();
+                    },
+                    function errorCallback(response) {
+                        $scope.$parent.flash_message = "See errors";
+                        $scope.$parent.flash_level = "fail";
+                        $scope.errors = {};
+                        var responseError;
+                        for (var i=0; i < response.data.errors.length; i++) {
+                            responseError = response.data.errors[i];
+                            if(responseError.hasOwnProperty('param')) {
+                                if(!$scope.errors[responseError['param']]) {
+                                    $scope.errors[responseError['param']] = [];
+                                }
+                                $scope.errors[responseError['param']].push(responseError['msg']);
+                            }
+                        }
+                    }
+                );
+            };
+        }else{
+            $scope.nickname = null;
+            $scope.phone_number = null;
+            $scope.birthday = null;
+            $scope.status = null;
+            $scope.bio = null;
+
+            $scope.submit = function() {
+                $http({
+                    method: 'POST',
+                    url: userProfileApiBaseURL + '/users/' + $routeParams.user_id,
+                    headers: {
+                        'x-access-token': CommonFunctions.getToken()
+                    },
+                    data: {
+                        nickname: $scope.nickname, 
+                        phone_number: $scope.phone_number, 
+                        birthday: $scope.birthday, 
+                        status: $scope.status, 
+                        bio: $scope.bio
+                    } 
+                }).then(
+                    function successCallback(response) {
+                        $localStorage.flash_message = "User Profile Updated!";
+                        $scope.$parent.flash_level = "success";
+                        window.history.back();
+                    },
+                    function errorCallback(response) {
+                        $scope.$parent.flash_message = "See errors";
+                        $scope.$parent.flash_level = "fail";
+                        $scope.errors = {};
+                        var responseError;
+                        for (var i=0; i < response.data.errors.length; i++) {
+                            responseError = response.data.errors[i];
+                            if(responseError.hasOwnProperty('param')) {
+                                if(!$scope.errors[responseError['param']]) {
+                                    $scope.errors[responseError['param']] = [];
+                                }
+                                $scope.errors[responseError['param']].push(responseError['msg']);
+                            }
+                        }
+                    }
+                );
+            };
         }
     });
 
-    $scope.put = function(){
-        if ($scope.not_exist()){
-            $http({
-                method: 'PUT',
-                url: usersApiBaseURL + '/users/' + $routeParams.user_id + '/password',
-                headers: {
-                    'x-access-token': CommonFunctions.getToken()
-                },
-                data: {password: $scope.password}
-            }).then(
-                function successCallback(response) {
-                    $localStorage.flash_message = "Successfully updated password!";
-                    $scope.$parent.flash_level = "success";
-                    window.history.back();
-                },
-                function errorCallback(response) {
-                    $scope.$parent.flash_message = "Error updating password.";
-                    $scope.$parent.flash_level = "fail";
-                    $scope.errors = {};
-                    var responseError;
-                    for (var i=0; i < response.data.errors.length; i++) {
-                        responseError = response.data.errors[i];
-                        if(responseError.hasOwnProperty('param')) {
-                            if(!$scope.errors[responseError['param']]) {
-                                $scope.errors[responseError['param']] = [];
-                            }
-                            $scope.errors[responseError['param']].push(responseError['msg']);
-                        }
-                    }
-                }
-            );
-        }
+        
+    }else {
+        $localStorage.flash_message = "Invalid Credentials";
+        $scope.$parent.flash_level = "fail";
+        $location.path('/');
     }
 });
