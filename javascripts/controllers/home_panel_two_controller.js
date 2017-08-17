c45e6808f54d8a551ce10bbffa49b0895fa45eb0
Ken Le <ken@ryukyu-i.co.jp>
2017-08-17 14:41:33 +0700

    Finish first iteration home panel 2

--- javascripts/controllers/home_panel_two_controller.js
+++ javascripts/controllers/home_panel_two_controller.js
@@ -11,48 +11,57 @@ app.controller('homePanelTwoController', function($scope, $http, $location, $loc
         $localStorage.tasks_limit = null;
         $localStorage.tasks_current_page = null;
         //delete $localStorage.tasks_maximized; //Save between sessions
+        
     } 
 
     CommonFunctions.setFlashMessage($scope, $localStorage);
     CommonFunctions.checkLoggedInUser($scope, $localStorage, $location);
 
+    $scope.capitalize = function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    $scope.statuses = ["dump","waiting","doing","finished"];
+
+    $scope.showByPerson = function($event) {
+        $localStorage.showBy = "person";
+        $scope.showBy = "person";
+    }
+
+    $scope.showByStatus = function($event) {
+        $localStorage.showBy = "status";
+        $scope.showBy = "status";
+    }
+
+    //Create container to save
+    if(!$localStorage.expand_projects) {
+        $localStorage.expand_projects = {}
+    }
+
+    if(!$scope.expand_projects) {
+        $scope.expand_projects = {}
+    }
+
+    //Load default show by settings
+    if($localStorage.showBy != undefined) {
+        $scope.showBy = $localStorage.showBy;
+    } else {
+        $localStorage.showBy = "status";
+        $scope.showBy = "status";
+    }
+
+    $scope.expand = function($event, category) {
+        $localStorage.expand_projects[category] = true;
+        $scope.expand_projects[category] = true;
+    }
+
+    $scope.collapse = function($event, category) {
+        $localStorage.expand_projects[category] = false;
+        $scope.expand_projects[category] = false;
+    }
+    
     if(!$localStorage.hasOwnProperty('tasks_maximized')) {
         $localStorage.tasks_maximized = true;
-    }
-
-    $scope.expandCollapse = function($event, status) {
-        if($($event.target).hasClass("collapse")) {
-            $($event.target).removeClass("collapse");
-            $($event.target).addClass("expand");
-            $localStorage.collapseSettings[status] = "expand";
-        } else {
-            $($event.target).removeClass("expand");
-            $($event.target).addClass("collapse");            
-            $localStorage.collapseSettings[status] = "collapse";
-        }
-    }
-
-    $scope.collapseSettingCSS = function(status) {
-        if(!$localStorage.collapseSettings) {
-            $localStorage.collapseSettings = {}
-        }
-
-        if($localStorage.collapseSettings && $localStorage.collapseSettings[status]) {
-            //Do nothing
-        } else {
-            //Set default value
-            $localStorage.collapseSettings[status] = "collapse";
-        }
-
-        if($localStorage.collapseSettings[status] == "expand") {
-            $(".preview.projects."+status).css("display","none");
-        } else {
-            $(".preview.projects."+status).css("display","block");
-        }
-
-       return $localStorage.collapseSettings[status];
-
-       return "collapse";
     }
 
     $scope.tasksMinMaxButtonClass = function() {
@@ -322,12 +331,29 @@ app.controller('homePanelTwoController', function($scope, $http, $location, $loc
             }
         }).then(function (response) {
             $scope.projects[user_id].data = response.data;
+
+            //Load default expand settings
+            if($localStorage.expand_projects[user_id] != undefined) {
+                $scope.expand_projects[user_id] = $localStorage.expand_projects[user_id];
+            } else {
+               $localStorage.expand_projects[user_id] = true; 
+               $scope.expand_projects[user_id] = true;
+            }
         });
     }
 
     //Get Projects
     $scope.getProjects = function(status,limit,page) {
-        //Save/default settings
+        //Load default expand/collapse settings
+        if($localStorage.expand_projects[status] != undefined) {
+            $scope.expand_projects[status] = $localStorage.expand_projects[status];
+        } else {
+            //If expand is defined, save the value
+            $localStorage.expand_projects[status] = true;
+            $scope.expand_projects[status] = true;
+        }
+
+        //Create containers for saving pagination parameters
         if(!$scope.projects[status]) {
             $scope.projects[status] = {}
         }
@@ -339,21 +365,24 @@ app.controller('homePanelTwoController', function($scope, $http, $location, $loc
         if(!$localStorage.projects[status]) {
             $localStorage.projects[status] = {}
         }
-
         if(!limit || limit == null || limit == undefined) {
+            //Default values if limit is not defined
             $localStorage.projects[status].limit = $scope.limits[1]; //For saving
             $scope.projects[status].limit = $scope.limits[1]; //For html
             limit = $scope.limits[1];
         } else {
+            //If limit is defined, save the limit value
             $localStorage.projects[status].limit = limit; //For saving
             $scope.projects[status].limit = limit; //For html
         }
 
         if(!page || page == null || page == undefined) {
+            //Default page if not defined
             $scope.projects[status].current_page = 1;
             $localStorage.projects[status].current_page = 1;
             page = 1;
         } else {
+            //If page is defined save the value
             $localStorage.projects[status].current_page = page;
             $scope.projects[status].current_page = page;
         }
